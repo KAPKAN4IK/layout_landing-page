a2759057d1558c974c2e0abacd7350402af4e6a5
Vasyl <vasja.tovtun@gmail.com>
2025-12-21 14:43:29 +0100

    message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const burgerButtonClose = document.querySelector('[data-burger="close"]');
+const burgerButtonOpen = document.querySelector('[data-burger="open"]');
+const aside = document.querySelector('.aside');
+
+burgerButtonClose.addEventListener('click', () => {
+  aside.classList.remove('aside--active');
+});
+
+burgerButtonOpen.addEventListener('click', () => {
+  aside.classList.add('aside--active');
+});
